57bad076f92062d56811bd4940ffdcd006bc4f8f
silas alarapon <alarapontope04@gmail.com>
2022-06-21 23:16:09 +0100

    first commit

---
+++ SilasAlarapon20/blog/views.py
@@ -0,0 +1,24 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from blog.models import Post
+class PostListView(ListView):
+    model = Post
+class PostCreateView(CreateView):
+    model = Post
+    fields =("__all__")
+    success_url: reverse_lazy("blog:all")
+class PostDetailView(DetailView):
+    model = Post
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ("__all__")
+    success_url = reverse_lazy("blog:all")
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = ("__all__")
+    success_url = reverse_lazy("blog:all")
+# Create your views here.
